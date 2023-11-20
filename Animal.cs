db2be7284602834a5c3aa8a36afddbbf8aebe8e4
OMFrydenlund <migfrydenlund@gmail.com>
2023-11-20 13:09:55 +0100

    Added comments

---
+++ Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetProject
+{
+    class Animal
+    {
+        public Animal(string name, string type, int age)
+        {
+            Name = name;
+            Type = type;
+            Age = age;
+        }
+        private string Name;
+        private int Age;
+        private string Type;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿// Add/take care of animal prompt (Press 1 or 2)
+
+// If 1st, 3 conseq question where user writes desired animal attributes
+
+// If 2nd, write line asking user to pick a listed animal
+
+// Lines that ask the user questions relevant to an executed task
+// Read user input converted to animal attributes
