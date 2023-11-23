cc50c892905b0fdc2daca3c3eb5d0fdd4d56f793
tobias <tobias@refsholt.net>
2023-11-23 14:41:19 +0100

    Countermeasure for faulty input

--- Game.cs
+++ Game.cs
@@ -10,7 +10,7 @@ public class Game
     {
         Console.Clear();
         Console.WriteLine("Add/take care of animal (Press 1 or 2) ");
-        var input = Convert.ToInt32(Console.ReadLine());
+        var input = GetIntInput(2, "Can't do that!");
         switch (input)
         {
             case 1:
@@ -52,14 +52,14 @@ public class Game
             Console.WriteLine($"({index}) {animal.Name} - Type: {animal.Type} - Age: {animal.Age} - Happiness: {animal.HappinessMeter}");
         }
         Console.WriteLine("Which animal would you like to take care of? ");
-        var selectedIndex = Convert.ToInt32(Console.ReadLine());
+        var selectedIndex = GetIntInput(AnimalPen.Count-1, "You dont have that many animals, try again");
         Console.Clear();
         var selectedAnimal = AnimalPen[selectedIndex];
         Console.WriteLine($"How would you like to take care of {selectedAnimal.Name} the {selectedAnimal.Type}? ");
         Console.WriteLine($"(1) Feed {selectedAnimal.Name}? ");
         Console.WriteLine($"(2) Snuggle with {selectedAnimal.Name}? ");
         Console.WriteLine($"(3) Take {selectedAnimal.Name} out to poop? ");
-        var selectedAction = Convert.ToInt32(Console.ReadLine());
+        var selectedAction = GetIntInput(3);
         switch (selectedAction)
         {
             case 1:
@@ -76,5 +76,21 @@ public class Game
         Console.Write("Press enter to continue... ");
         Console.ReadLine();
         Run();
+    }
+
+    private static int GetIntInput(int max, string errorMessage = "Number out of range!")
+    {
+        var input = Console.ReadLine();
+        try
+        {
+            var number = int.Parse(input);
+            if (number >= 0 && number <= max) return number;
+            Console.WriteLine(errorMessage);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Not a number, try again.");
+        }
+        return GetIntInput(max);
     }
 }
